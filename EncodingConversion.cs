67c6b3f21008b71635fd7225e61296d534dea8c9
Ace Poker <2549991372@qq.com>
2021-09-03 13:18:04 +0800

    更改了编码转换

--- EncodingConversion.cs
+++ EncodingConversion.cs
@@ -5,17 +5,75 @@ namespace NaflimHelperLibrary
 {
     public class EncodingConversion
     {
+        Encoding utf = Encoding.GetEncoding("utf-8");
+        Encoding gbk = Encoding.GetEncoding("gb2312");
+
         /// <summary>
-        /// 字符串转uft-8
+        /// gbk转utf-8
         /// </summary>
-        /// <param name="unicodeString"></param>
-        /// <returns></returns>
-        public static string Get_utf8(string unicodeString)
+        /// <param name="txt">转换文本</param>
+        /// <returns>转换后字符串</returns>
+        public string GBKofUTF(string txt)
         {
-            UTF8Encoding utf8 = new UTF8Encoding();
-            Byte[] encodedBytes = utf8.GetBytes(unicodeString);
-            String decodedString = utf8.GetString(encodedBytes);
-            return decodedString;
+            return TransferEncoding(gbk, utf, txt);
+        }
+
+        /// <summary>
+        /// gbk转utf-8
+        /// </summary>
+        /// <param name="txt">转换文本</param>
+        /// <returns>转换后字符串</returns>
+        public byte[] GBKofUTFBys(string txt)
+        {
+            return TransferEncodingBys(gbk, utf, txt);
+        }
+
+        /// <summary>
+        /// utf-8转gbk
+        /// </summary>
+        /// <param name="txt">转换文本</param>
+        /// <returns>转换后字符串</returns>
+        public string UTFofGBK(string txt)
+        {
+            return TransferEncoding(utf, gbk, txt);
+        }
+
+        /// <summary>
+        /// utf-8转gbk
+        /// </summary>
+        /// <param name="txt">转换文本</param>
+        /// <returns>转换后字节数组</returns>
+        public byte[] UTFofGBKBys(string txt)
+        {
+            return TransferEncodingBys(utf, gbk, txt);
+        }
+
+        /// <summary>
+        /// 字符串编码转换
+        /// </summary>
+        /// <param name="srcEncoding">原编码</param>
+        /// <param name="dstEncoding">目标编码</param>
+        /// <param name="srcStr">原字符串</param>
+        /// <returns>转换后字符串</returns>
+        public static string TransferEncoding(Encoding srcEncoding, Encoding dstEncoding, string srcStr)
+        {
+            byte[] srcBytes = srcEncoding.GetBytes(srcStr);
+            byte[] bytes = Encoding.Convert(srcEncoding, dstEncoding, srcBytes);
+            return dstEncoding.GetString(bytes);
+        }
+
+        /// <summary>
+        /// 字符串编码转换
+        /// </summary>
+        /// <param name="srcEncoding">原编码</param>
+        /// <param name="dstEncoding">目标编码</param>
+        /// <param name="srcStr">原字符串</param>
+        /// <returns>转换后字节数组</returns>
+        public static byte[] TransferEncodingBys(Encoding srcEncoding, Encoding dstEncoding, string srcStr)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            byte[] srcBytes = srcEncoding.GetBytes(srcStr);
+            return Encoding.Convert(srcEncoding, dstEncoding, srcBytes);
         }
     }
 }
