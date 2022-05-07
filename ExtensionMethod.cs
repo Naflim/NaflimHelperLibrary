cdb3fadc9e10fbb04d5a2e3c598599acb5a7f953
Naflim <naflim@outlook.com>
2022-05-07 18:23:07 +0800

    .

---
+++ ExtensionMethod.cs
@@ -0,0 +1,42 @@
+﻿/*************************************************************************************
+ *
+ * 文 件 名:   ExtensionMethod
+ * 描    述:   扩展方法类
+ * 
+ * 版    本：  V1.0
+ * 创 建 者：  Naflim 
+ * 创建时间：  2022/5/7 15:50:03
+ * ======================================================
+ * 历史更新记录
+ * 版本： V          修改时间：         修改人：
+ * 修改内容：
+ * ======================================================
+*************************************************************************************/
+
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace NaflimHelperLibrary
+{
+    public static class ExtensionMethod
+    {
+        /// <summary>
+        /// 深度克隆，字典和List可以直接.Clone，如果是类的话那个类必须得打上[Serializable]标签
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="RealObject"></param>
+        /// <returns></returns>
+        public static T Clone<T>(this T RealObject)
+        {
+            using (Stream objectStream = new MemoryStream())
+            {
+                //利用 System.Runtime.Serialization序列化与反序列化完成引用对象的复制  
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectStream, RealObject);
+                objectStream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(objectStream);
+            }
+        }
+    }
+}
--- TypeConversion.cs
+++ TypeConversion.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace NaflimHelperLibrary
 {
@@ -11,7 +12,7 @@ namespace NaflimHelperLibrary
         /// 对象转json
         /// </summary>
         /// <param name="obj">转换对象</param>
-        /// <returns></returns>
+        /// <returns>json字符串</returns>
         public static string ObjectToJson(object obj)
         {
             return JsonConvert.SerializeObject(obj);
@@ -22,10 +23,44 @@ namespace NaflimHelperLibrary
         /// </summary>
         /// <typeparam name="T">对象类型</typeparam>
         /// <param name="json">json字符串</param>
-        /// <returns></returns>
+        /// <returns>转换对象</returns>
         public static T JsonToObject<T>(string json)
         {
             return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        /// <summary>
+        /// 将字典类型序列化为json字符串
+        /// </summary>
+        /// <typeparam name="TKey">字典key</typeparam>
+        /// <typeparam name="TValue">字典value</typeparam>
+        /// <param name="dict">要序列化的字典数据</param>
+        /// <returns>json字符串</returns>
+        public static string DictionaryToJson<TKey, TValue>(Dictionary<TKey, TValue> dict)
+        {
+            if (dict.Count == 0)
+                return string.Empty;
+
+            string jsonStr = JsonConvert.SerializeObject(dict);
+            return jsonStr;
+        }
+
+        /// <summary>
+        /// 将json字符串反序列化为字典类型
+        /// </summary>
+        /// <typeparam name="TKey">字典key</typeparam>
+        /// <typeparam name="TValue">字典value</typeparam>
+        /// <param name="jsonStr">json字符串</param>
+        /// <returns>字典数据</returns>
+        public static Dictionary<TKey, TValue> JsonToDictionary<TKey, TValue>(string jsonStr)
+        {
+            if (string.IsNullOrEmpty(jsonStr))
+                return new Dictionary<TKey, TValue>();
+
+            Dictionary<TKey, TValue> jsonDict = JsonConvert.DeserializeObject<Dictionary<TKey, TValue>>(jsonStr);
+
+            return jsonDict;
+
         }
     }
 }
